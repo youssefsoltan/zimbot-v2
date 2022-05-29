b93eee41c24a542cfd998bc84bfbe9844f32aeb7
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2022-05-29 03:12:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,15 +33,15 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['201288455338',''] //owner number +201288455338
+global.pemilik = ['201288455338'] //owner number +201288455338
+global.premium = ['201288455338'] //premium number +201288455338
+global.pengguna = 'AL SOLTAN' //username
+global.footer = 'AL SOLTAN'
+global.botnma = 'BOT AL SOLTAN' //bot name
+global.ownernma = '‘ Youssef Al Soltan,' //owner name
+global.packname = '@201288455338' //sticker package name
+global.author = 'By bot al soltan' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
